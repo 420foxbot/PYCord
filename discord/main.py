70692aa690a8da4ab839158c4377b1535d467120
Joseph Banks <josephbanks7@gmail.com>
2016-10-23 23:08:58 +0100

    It might work

--- discord/main.py
+++ discord/main.py
@@ -1,6 +1,24 @@
 import http.client
-
+import json
 conn = http.client.HTTPSConnection("discordapp.com")
 
 class Client:
     """A Discord Client"""
+    def login(self,token):
+        if token:
+            self.token = token
+        else:
+            print("Could not login")
+            exit()
+    def sendMessage(self,id,content):
+        payload = {
+            "content":content
+        }
+
+        headers = {
+            'authorization': self.token,
+            'content-type': "application/json",
+            'cache-control': "no-cache"
+        }
+
+        conn.request("POST", "/api/channels/112233445566778899/messages", json.dumps(payload), headers)
