c1754f3e41b63610ca8109240a86f0299607d0e3
Joseph Banks <josephbanks7@gmail.com>
2016-10-23 23:44:54 +0100

    I FORGOT TO REMOVE THE EXAMPLE CHANNEL ID

--- discord/__init__.py
+++ discord/__init__.py
@@ -18,7 +18,7 @@ class Client:
             'cache-control': "no-cache"
         }
 
-        conn.request("POST", "/api/channels/112233445566778899/messages", json.dumps(payload), headers)
+        conn.request("POST", "/api/channels/{}}/messages".format(id), json.dumps(payload), headers)
 
         data = conn.getresponse()
         res = data.read()
